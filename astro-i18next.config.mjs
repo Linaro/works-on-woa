f02207d8b6b650d0a60755aa1849a75e43c5e666
franfels <121124363+franfels@users.noreply.github.com>
2025-04-24 13:53:54 -0300

    fix: fails to display localized strings for any language (#17)

--- astro-i18next.config.mjs
+++ astro-i18next.config.mjs
@@ -1,4 +1,8 @@
 import { DEFAULT_LOCALE, LOCALES } from "./src/config/i18nConfig.js";
+import en from "./public/locales/en/translation.json" assert { type: 'json' };
+import ja from "./public/locales/ja/translation.json" assert { type: 'json' };
+import ko from "./public/locales/ko/translation.json" assert { type: 'json' };
+import zh from "./public/locales/zh/translation.json" assert { type: 'json' };
 
 export default {
   defaultLocale: DEFAULT_LOCALE,
@@ -8,5 +12,13 @@ export default {
   showDefaultLocale: true,
   i18nextServer: {
     debug: true,
-  }
+    resources: {
+      en: { translation: { ...en } },
+      ja: { translation: { ...ja } },
+      ko: { translation: { ...ko } },
+      zh: { translation: { ...zh } },
+    },
+    ns: ['translation'],
+    defaultNS: 'translation'
+  },
 };
