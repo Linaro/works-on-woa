ac7912689e13ee6c7e4fc3ed8dd69081a238b722
Philip Colmer <philip.colmer@linaro.org>
2025-07-09 10:49:27 +0100

    Log error info

--- get_aws_creds.py
+++ get_aws_creds.py
@@ -37,7 +37,9 @@ def vault_login(role_id, secret_id, aws_role):
         access_key = result["data"]["access_key"]
         secret_key = result["data"]["secret_key"]
         security_token = result["data"]["security_token"]
-    print(r.text)
+    else:
+        print(role_id, secret_id, aws_role, file=sys.stderr)
+        print(r.text, file=sys.stderr)
     # Now revoke the client token
     requests.post(
         "https://login.linaro.org:8200/v1/auth/token/revoke-self",
